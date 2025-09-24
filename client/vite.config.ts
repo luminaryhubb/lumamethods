c7a65ae69e815ac970db82c62bda8266480f9b51
luminaryhubb <laccyoficial@gmail.com>
2025-09-24 19:35:40 -0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../server/dist", // joga o build direto para o server servir
+    outDir: "../server/dist", // build do client vai para o backend
     emptyOutDir: true
-  },
-  server: {
-    port: 5173
   }
 });
