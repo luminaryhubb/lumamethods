73b748528a904479793c83074c2e14f8e1802de1
luminaryhubb <laccyoficial@gmail.com>
2025-09-25 18:41:38 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './client',  // Certifique-se de que o caminho para o root esteja correto
+  build: {
+    outDir: 'dist',  // Onde o build será gerado
+  },
   server: {
-    host: true,  // Permite acessar o frontend no endereço correto
-    port: 5173   // A porta padrão para o Vite
+    host: true,  // Permite o acesso externo ao frontend
+    port: 5173
   }
 });
