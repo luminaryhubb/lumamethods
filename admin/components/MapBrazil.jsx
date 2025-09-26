23e35d150d29f916916476deba45f864f2cefd8e
luminaryhubb <laccyoficial@gmail.com>
2025-09-26 20:11:55 -0300

    Delete admin directory

--- admin/components/MapBrazil.jsx
+++
@@ -1,7 +0,0 @@
-window.MapBrazil = () => {
-  return (
-    <div className="bg-neutral-800 rounded p-6 flex items-center justify-center" style={{height:300}}>
-      <div className="text-gray-400">[Mapa do Brasil - placeholder]</div>
-    </div>
-  );
-};
