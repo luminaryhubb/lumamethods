dc215bcb03baae99e16ea7581a1807d35bbd343f
luminaryhubb <laccyoficial@gmail.com>
2025-09-27 18:38:47 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,4 @@
 // src/pages/Dashboard/index.js
-import React from 'react'
+import Dashboard from './Dashboard.jsx'
 
-export default function Dashboard() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p>Bem-vindo ao painel!</p>
-    </div>
-  )
-}
+export default Dashboard
